54d9df98ab124f6b6fccf52bb622ecf66fdd1019
ForestW70 <hexaforest@gmail.com>
2021-03-21 13:38:40 -0400

    initial app building, mostly html/css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,46 @@
-console.log("welcome to scriptville");
+const timeBlockForm = $('#9form');
+const timeBlockInput = $('#9am');
+const timeBlockButton = $('#9amButton');
+
+let fivePmField = $('#5pm');
+const saveButton = $('.btn');
+
+let message = fivePmField.value;
+
+saveButton.on("click", function(e) {
+    e.preventDefault();
+    // let message = fivePmField.value.trim();
+    console.log(message);
+
+})
+
+// let saveMeeting = function() {
+//     let dailyMeetings = {
+//         meeting:  fourPmField.value
+//     }
+
+//     localStorage.setItem('dailyMeetings', JSON.stringify(dailyMeetings));
+// }
+
+// let renderMeeting = function() {
+//     let currentEvent = JSON.parse(localStorage.getItem("dailyMeetings"));
+//     fourPmField.innerText = currentEvent.meeting;
+
+// }
+
+
+// saveButton.on('click', function(e) {
+//     e.preventDefault();
+//     saveMeeting();
+//     renderMeeting();
+// });
+
+
+var today = moment();
+$("#currentDay").text(today.format("[Today's date is] MMM Do, YYYY"));
+
+var timeRightNow = moment();
+$("#currentTime").text(today.format("[It is currently] LTS"));
+
+
+// $('.container-fluid').find
