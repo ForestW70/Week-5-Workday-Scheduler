09546f6675471bd60b0d10510a35e19905ebe672
Forest wilson <hexaforest@gmail.com>
2021-03-23 17:56:52 -0400

    added hour colors that change based on what the time of day is

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,31 @@
-const timeBlockForm = $('#9form');
-const timeBlockInput = $('#9am');
-const timeBlockButton = $('#9amButton');
 
-let fivePmField = $('#5pm');
 const saveButtons = document.querySelectorAll(".btn");
+const timeBlocks = document.querySelectorAll("li");
 
-let message = fivePmField.value;
+let today = moment();
+let currentHour = today.hour();
 
-// saveButton.on("click", function(e) {
-//     e.preventDefault();
-//     // let message = fivePmField.value.trim();
-//     console.log(message);
 
-// })
+
+
+timeBlocks.forEach(function(hourLi, index) {
+    if (index+12 < currentHour) {
+        hourLi.setAttribute("class", "past");
+    } else if (index+12 == currentHour) {
+        hourLi.setAttribute("class", "present");
+    } else {
+        hourLi.setAttribute("class", "future");
+    }
+
+    if (currentHour == 0) {
+        hourLi.setAttribute("class", "");
+    }
+})
+
 
 let saveMeeting = function(e) {
     const data = JSON.parse(localStorage.getItem("dailyMeetings")) || {};
     const textArea = e.target.previousElementSibling;
-    
     
     data[textArea.id] = textArea.value;
 
@@ -27,17 +35,16 @@ let saveMeeting = function(e) {
 let renderMeeting = function() {
     let currentEvent = JSON.parse(localStorage.getItem("dailyMeetings")) || {};
 
-    Object.keys(currentEvent).forEach(key => {
+    Object.keys(currentEvent).forEach(function(key) {
         document.getElementById(key).value = currentEvent[key]
     })
-    
 }
+
 
 renderMeeting();
 
 
-
-saveButtons.forEach((element) => {
+saveButtons.forEach(function (element) {
     element.addEventListener("click", (e) => {
         e.preventDefault();
         saveMeeting(e);
@@ -49,13 +56,11 @@ saveButtons.forEach((element) => {
 
 
 
-var today = moment();
+
 $("#currentDay").text(today.format("[Today's date is] MMM Do, YYYY"));
 
-var timeRightNow = moment();
-$("#currentTime").text(today.format("[It is currently] LTS"));
+$("#currentTime").text(today.format("[It is currently] LT"));
 
 
 
 
-// $('.container-fluid').find
