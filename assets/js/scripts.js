c0ffd5a4166346750914b2ab6e919bff9646f32e
Forest wilson <hexaforest@gmail.com>
2021-03-23 13:18:34 -0400

    hopefully got my local storage to start working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,37 +3,50 @@ const timeBlockInput = $('#9am');
 const timeBlockButton = $('#9amButton');
 
 let fivePmField = $('#5pm');
-const saveButton = $('.btn');
+const saveButtons = document.querySelectorAll(".btn");
 
 let message = fivePmField.value;
 
-saveButton.on("click", function(e) {
-    e.preventDefault();
-    // let message = fivePmField.value.trim();
-    console.log(message);
+// saveButton.on("click", function(e) {
+//     e.preventDefault();
+//     // let message = fivePmField.value.trim();
+//     console.log(message);
 
+// })
+
+let saveMeeting = function(e) {
+    const data = JSON.parse(localStorage.getItem("dailyMeetings")) || {};
+    const textArea = e.target.previousElementSibling;
+    
+    
+    data[textArea.id] = textArea.value;
+
+    localStorage.setItem('dailyMeetings', JSON.stringify(data));
+}
+
+let renderMeeting = function() {
+    let currentEvent = JSON.parse(localStorage.getItem("dailyMeetings")) || {};
+
+    Object.keys(currentEvent).forEach(key => {
+        document.getElementById(key).value = currentEvent[key]
+    })
+    
+}
+
+renderMeeting();
+
+
+
+saveButtons.forEach((element) => {
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+        saveMeeting(e);
+        renderMeeting();
+    })
 })
 
-// let saveMeeting = function() {
-//     let dailyMeetings = {
-//         meeting:  fourPmField.value
-//     }
-
-//     localStorage.setItem('dailyMeetings', JSON.stringify(dailyMeetings));
-// }
-
-// let renderMeeting = function() {
-//     let currentEvent = JSON.parse(localStorage.getItem("dailyMeetings"));
-//     fourPmField.innerText = currentEvent.meeting;
-
-// }
 
 
-// saveButton.on('click', function(e) {
-//     e.preventDefault();
-//     saveMeeting();
-//     renderMeeting();
-// });
 
 
 var today = moment();
@@ -41,6 +54,8 @@ $("#currentDay").text(today.format("[Today's date is] MMM Do, YYYY"));
 
 var timeRightNow = moment();
 $("#currentTime").text(today.format("[It is currently] LTS"));
+
+
 
 
 // $('.container-fluid').find
